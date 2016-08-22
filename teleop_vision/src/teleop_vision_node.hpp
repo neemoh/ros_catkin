e762f56814d8d5c4343d575591a021103c2b16e6
nima <nima.en@gmail.com>
2016-08-22 12:27:39 +0200

    correct path generation switching  and calculate circumference

--- teleop_vision/src/teleop_vision_node.hpp
+++ teleop_vision/src/teleop_vision_node.hpp
@@ -19,6 +19,7 @@
 #include "kdl/frames.hpp"
 #include "geometry_msgs/Pose.h"
 #include <tf_conversions/tf_kdl.h>
+#include <std_msgs/Char.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
@@ -29,6 +30,7 @@ using namespace cv;
 
 
 
+const Scalar RED(50,0,255), GREEN1(30,220,10), GREEN2(80,150,10), BLUE1(220,100,20), BLUE2(200,130,20), CYAN(255,255,0), ORANGE(65,64,255);
 
 //-----------------------------------------------------------------------------------
 // BOARD DETECtOR CLASS
@@ -94,19 +96,21 @@ public:
 	void init();
 	void toolCurrCallback(const geometry_msgs::Pose::ConstPtr& msg);
 	void toolDestCallback(const geometry_msgs::Pose::ConstPtr& msg);
-
+	void eventsCallback(const std_msgs::Char::ConstPtr& msg);
 public:
 	bool all_good;
 	std::string cam_data_path_param;
 	std::string tool_curr_topic_name_param;
 	std::string tool_dest_topic_name_param;
+	std::string events_topic_name_param;
 	std::string node_name;
 	ros::NodeHandle n;
 	double freq_ros;
 	int camId_param;
+	std_msgs::Char acquisition_event;
 //	ros::Subscriber sub_robot;
-	geometry_msgs::Pose tool_curr_pose;
-	geometry_msgs::Pose tool_dest_pose;
+	geometry_msgs::Pose tool_curr_pose_in_slvrf;
+	geometry_msgs::Pose tool_dest_pose_in_slvrf;
 	sensor_msgs::PointCloud2 ac_point_cloud;
 	vector<double> board_to_robot_tr_param;
 	int markersX_param;
@@ -177,20 +181,52 @@ public:
 
 	// drawing a simple rectangle
 	void create3dCurve(double _center_x, double _center_y, unsigned int n_points);
-	void createCircle(double _center_x, double _center_y, double a, double b, unsigned int n_points);
+
+	// create an ellipse save in ac_points_in_board and convert and save in ac_points_in_robot
+	void createEllipse(double _center_x, double _center_y, double a, double b, unsigned int n_points);
+
+	// create an ellipse save in ac_points_in_board and convert and save in ac_points_in_robot
+	void createSine(double _center_x, double _center_y, double a, double b, unsigned int n_points);
+
+	// create an ellipse save in ac_points_in_board and convert and save in ac_points_in_robot
+	void createHat(double _center_x, double _center_y, double a, double b, unsigned int n_points);
 
 	void drawSquare(InputOutputArray _image);
+
 	void drawToolTip(InputOutputArray _image, double _x, double _y, double _z, const Scalar _color);
 
+	// showing the projection of the current tool pose to the desired point
 	void drawGuidingLines(InputOutputArray _image, const geometry_msgs::Pose & tool, const geometry_msgs::Pose & desired);
 
-	void drawACPoints(InputOutputArray _image);
+	void drawACPoints(InputOutputArray _image, bool _is_tool_in_touch);
+
 	void update_cam_2_board_ref(Vec3d _bc_rvec,	Vec3d _bc_tvec){
 		bc_rvec = _bc_rvec; bc_tvec = _bc_tvec;
 	}
 
+	// generate a pointCloud2 message from the ac_points_in_robot
 	void getACCloud(sensor_msgs::PointCloud2 & cloud_out);
 
+	// transform and save to curr_tool_boardrf and dest_tool_boardrf
+	void transfromToBoard(const geometry_msgs::Pose & _tool_curr_in_slvrf, const geometry_msgs::Pose & _tool_dest_in_slvrf);
+
+	// save the position and the color of the users position
+	// color is from blue to red showing the amount of penetration in z.
+	void recordUsersPath(bool _is_tool_in_touch);
+
+	// draw the points with the colors recorded as the user path
+	void drawUsersPath(InputOutputArray _image);
+
+	// resets the points and the colors saved as the user path
+	void resetUsersPath();
+
+	// set a notification that will be shown for _duration_counts counts.
+	void setTransientNotification(string _msg, double _origin_x, double _origin_y, unsigned int _duration_counts);
+
+	// show the transient notification
+	void showTransientNotification(InputOutputArray _image);
+
+	bool isToolInTouch(double _depth);
 public:
 
 	Mat camMatrix;
@@ -199,6 +235,12 @@ public:
 	Vec3d bc_rvec;
 	Vec3d bc_tvec;
 
+	Point3d curr_tool_boardrf;
+	Point3d dest_tool_boardrf;
+
+	vector<Point3d> users_path;
+	vector<Scalar> users_path_colors;
+	bool new_task;
 //	KDL::Frame br_frame;
 	Point3d br_trans;
 	Matx33d br_rotm;
@@ -210,6 +252,12 @@ public:
     vector< Point3d > sq_points_in_board;
     vector< Point3d > ac_points_in_board;
     vector< Point3d > ac_points_in_robot;
+
+
+    // messaging
+	unsigned int notification_counter;
+	string transient_notification_msg;
+	Point textOrigin;
 
 };
 
